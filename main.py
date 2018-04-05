bb2f64165b378ff3b7927c7a3ffa999c2923d3ff
tillisd <danielle.tillis0826@gmail.com>
2018-04-05 13:20:14 -0500

    updates

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+app =Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font:10px sans-serif;
+                border-radius: 10px;
+             }
+             textarea {
+                 margin: 10px 0;
+                 width: 540px; 
+                 height: 120px;
+            
+             }
+        </style>   
+    </head>
+    <body>
+        <form>
+            <input type="text" name="first_name" />
+            <input type="submit" />
+    </body>
+</html>
+"""
+
+
+@app.route("/")
+def index ():
+    return "Hello World"
+
+app.run()
