e1bb9c9c68ca22cf1cf67dbd2e7a83a4252ada05
tillisd <danielle.tillis0826@gmail.com>
2018-04-05 22:19:36 -0500

    Updating Caesar Web

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
-app =Flask(__name__)
-app.config['DEBUG'] = True
+app = Flask(__name__)
+app.config ['DEBUG'] = True
 
 form = """
 <!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font:10px sans-serif;
                 border-radius: 10px;
-             }
-             textarea {
+             }}
+             textarea {{
                  margin: 10px 0;
                  width: 540px; 
                  height: 120px;
             
-             }
+             }}                      
         </style>   
     </head>
     <body>
-        <form>
-            <input type="text" name="first_name" />
-            <input type="submit" />
+        <form method="POST">
+                <label for ="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0" />
+                <textarea name="text" >{0}</textarea>
+                <input type="submit" value="Submit Query">
+        </form>
     </body>
 </html>
+
 """
 
 
 @app.route("/")
+
 def index ():
-    return "Hello World"
+    return form.format("")
+
+
+@app.route("/", method=['POST'])
+def encrypt():
+    form_rot = int(request.form['rot'])
+    form_text = request.form['text']
+    encrypted = rotate_string(form_text,form_rot) 
+    return form.format(encrypted)
 
 app.run()
